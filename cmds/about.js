8f19fffdcda97fecc6251abe62c2813328c9f53f
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-05-19 20:32:21 +0200

    Add GitHub and Discord links

--- cmds/about.js
+++ cmds/about.js
@@ -10,7 +10,8 @@ function about(message, client, prefix) {
                 .addField("Technical information", `Libary used: Discord.js\nDelay between bot and Discord server: ${Math.round(client.ping)} ms\nRAM used: ${Math.round(process.memoryUsage().rss / 1024 / 1000)} MB\nUptime: ${UpTime}`)
                 .setThumbnail(`${client.user.avatarURL}`)
                 .setFooter(`${client.user.username} is created by Greep#3022`, `${client.user.avatarURL}`)
-            message.channel.send({ embed: embed })
+            message.channel.send(embed)
+            message.channel.send("If you are really interested in bot development, you can see the bot source code on GitHub: https://github.com/GreepTheSheep/CoronaLanterns \nYou can also join the bot development server: https://discord.gg/5QCQpr9")
     }
 }
 
