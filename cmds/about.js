df53e989c82198605b541b479f9da1196f766d56
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-05-24 14:09:38 +0200

    too much Greep#3022, fixed!

--- cmds/about.js
+++ cmds/about.js
@@ -5,12 +5,12 @@ function about(message, client, prefix) {
     if (message.content.startsWith(prefix + 'about')) {
         let embed = new Discord.RichEmbed()
             embed.setColor("#9C01C4")
-                .addField("About the bot:", `**${client.user.tag}**\nCreated by Greep#3022\nFor [the Kingdom of Corona ☀](https://discord.gg/BunQeKh)`)
+                .addField("About the bot:", `**${client.user.tag}**\nFor [the Kingdom of Corona ☀](https://discord.gg/BunQeKh)`)
                 .addField("Cast:", `- **Greep#3022** : Original idea\n- **Ajam#3536** : Contributor`, true)
                 .addField("Technical information", `Libary used: [Discord.js](https://discord.js.org)\nDelay between bot and Discord server: ${Math.round(client.ping)} ms\nRAM used: ${Math.round(process.memoryUsage().rss / 1024 / 1000)} MB\nUptime: ${UpTime}`)
                 .addField("Useful links:", `[Bot source code on GitHub]( https://github.com/GreepTheSheep/CoronaLanterns)\n[Developement server](https://discord.gg/5QCQpr9)`, true)
                 .setThumbnail(`${client.user.avatarURL}`)
-                .setFooter(`${client.user.username} is created by Greep#3022`, `${client.user.avatarURL}`)
+                .setFooter(`${client.user.username}`, `${client.user.avatarURL}`)
             message.channel.send(embed)
     }
 }
