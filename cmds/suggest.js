fd34e7f3a10eb81039b7c14e325571bc81764105
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-05-26 23:00:02 +0200

    Separate bot suggests and server suggests

--- cmds/suggest.js
+++ cmds/suggest.js
@@ -7,15 +7,17 @@ function suggest(message, client, prefix) {
 
         const args = message.content.split(" ").slice(1);
 
-        if (args.length < 1) {
-            return message.reply("Please enter your suggestion!")
+        if (args.length < 2) {
+            return message.reply("Usage: `!suggest [bot, server] [your suggestion]`")
         }
 
-        var args2 = message.content.split(' ').slice(1).join(' ');
+        var args2 = args.slice(1).join(' ');
 
-        const suggestchannel = client.guilds.get('570024448371982373').channels.get('579675497970270240')
+        const botsuggestchannel = client.guilds.get('570024448371982373').channels.get('579675497970270240')
+        const srvsuggestchannel = client.guilds.get('570024448371982373').channels.get('579675497970270240')
 
-        suggestchannel.send('', {
+        if (args[0] == 'bot'){
+        botsuggestchannel.send('', {
             embed: {
                 color: 654456,
                 author: {
@@ -34,7 +36,30 @@ _Suggest:_
 **${message.author.tag}**`,
 
             }
+        })}
+
+    if (args[0] == 'server'){
+        srvsuggestchannel.send('', {
+            embed: {
+                color: 654456,
+                author: {
+                    name: "A server suggestion has been posted!",
+                    icon_url: message.author.avatarURL,
+                },
+                title: "Server Suggest",
+
+                description: `_Sent in_
+        **${message.guild.name}**
+
+_Suggest:_
+**${args2}**
+
+         _Sent by_
+**${message.author.tag}**`,
+
+            }
         })
+    }
         message.delete()
 
         message.channel.send('Your suggestion has been posted! <:heureuse:570820764799074335>')
