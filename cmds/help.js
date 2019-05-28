1387c541c6416c6def876f086bfae37c51c37745
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-05-28 12:50:05 +0200

    Fix bug

--- cmds/help.js
+++ cmds/help.js
@@ -2,14 +2,13 @@ const Discord = require('discord.js');
 
 function help(message, client, prefix) {
     if (message.content.startsWith(prefix + 'help') || message.content.startsWith(prefix + 'commands')) {
-        let thumb = randomItem(images);
         let embed = new Discord.RichEmbed()
             embed.setTitle('You feel lost? Don\'t worry :wink:')
                 .setColor("#9C01C4")
                 .addField("Commands:", `\`${prefix}quote\` : Send a random quote from Tangled (and the series also)\n\`${prefix}lanterns\` : see how much lanterns are thown (launch your lantern using this emoji : <:Lantern:570822664789426186>)`, true)
                 .addField("If you have any problems :", `\`${prefix}bug\`: Report any bug to devs\nor\n\`${prefix}suggest\`: Suggest an improvement`, true)
                 .setFooter(`You can type ${prefix}about for credits!`, `${client.user.avatarURL}`)
-            message.channel.send({ embed: embed })
+            message.channel.send(embed)
     }
 }
 
