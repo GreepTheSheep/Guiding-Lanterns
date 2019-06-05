809cafcc3e4321032fe136598d27a24506e0d995
Greep <89matt89.md@gmail.com>
2019-06-05 23:04:13 +0200

    Separate members and bots

--- counter/member.js
+++ counter/member.js
@@ -3,8 +3,11 @@ const Discord = require('discord.js');
 //Update amount of members.
 function num_members(client) {
     const guild = client.guilds.get('570024448371982373');
+    const total = guild.members.array().length;
+    const bots = guild.members.filter(m => m.user.bot).size; 
+    const members = total - bots
     const channel = guild.channels.get('585782174012407848');
-    channel.setName(`Members: ${guild.memberCount}`).catch(err=>console.log(err));
+    channel.setName(`Members: ${members}`).catch(err=>console.log(err));
 }
 
 module.exports = num_members;
