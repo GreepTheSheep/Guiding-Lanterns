ea42ca782d8042e950f76ec667c52437c1703ccd
Greep <89matt89.md@gmail.com>
2019-06-05 22:56:13 +0200

    Refresh the Frozen II countdown every day

--- counter/frozen2.js
+++ counter/frozen2.js
@@ -6,7 +6,15 @@ function frozen_2_countdown(client) {
     var days = Math.floor( t/(1000*60*60*24) );
     const guild = client.guilds.get('570024448371982373');
     const channel = guild.channels.get('585834618910015491');
-    channel.setName(`${days} Days until Frozen II`).catch(err=>console.log(err));
+    const text = `${days} Days until Frozen II`
+    channel.setName(`${text}`).catch(err=>console.log(err));
+
+    const updatecount = new Promise(function(resolve, reject) {
+        setInterval(function() {
+            channel.setName(`${text}`).catch(err=>console.log(err));
+        }, 24 * 60 * 60 * 1000);
+    });
+    updatecount
 }
 
 module.exports = frozen_2_countdown;
