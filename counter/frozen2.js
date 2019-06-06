5e096f57fa70b73787bf959f5965f2a7f20c275f
Ajam <ajam3536@protonmail.com>
2019-06-06 06:27:38 +0000

    Revert "Merge branch 'feature/bot-server-stats' of github.com:GreepTheSheep/CoronaLanterns into feature/bot-server-stats"
    This reverts commit bd56f53dc720fd19a7d077dd3b49e13943c10c56, reversing
    changes made to 195be82750fb2a120fe5322a20ddb8720c26526c.

--- counter/frozen2.js
+++ counter/frozen2.js
@@ -1,23 +1,25 @@
 const Discord = require('discord.js');
 
 //Frozen II countdown
-function frozen_2_countdown(client) {
-    var t = Date.parse("November 22 2019 00:00:00 GMT-0400") - Date.parse(new Date());
-    const guild = client.guilds.get('570024448371982373');
-    const channel = guild.channels.get('585834618910015491');
-    const text = `${days} Days until Frozen II`
+function frozen_2_countdown(client,channel_id) {
+    const channel = guild.channels.get(channel_id);
+    if (!channel) {
+        console.log(`Channel: ${channel_id} cannot be found`);
+        return;
+    }
 
-    var x = setInterval(function() {
-        var days = Math.floor( t/(1000*60*60*24) );
-        channel.setName(`${text}`).catch(err=>console.log(err));
-        console.log(`Frozen II countdown refreshed!\n${text}`)
-    
-        if (t < 0) {
-            clearInterval(x);
-            channel.setName(`FROZEN II IS OUT!!`).catch(err=>console.log(err));
-            console.log(`Frozen II countdown refreshed!\nFROZEN II IS OUT!!`)
-        }
-    }, 1000);
+    var t = Date.parse("November 22 2019 00:00:00 GMT-0400") - Date.parse(new Date());
+    var days = Math.floor( t/(1000*60*60*24) );
+    const text = `${days} Days until Frozen II`
+    channel.setName(`${text}`).catch(err=>console.log(err));
+
+    const updatecount = new Promise(function(resolve, reject) {
+        setInterval(function() {
+            channel.setName(`${text}`).catch(err=>console.log(err));
+            console.log(`Frozen II countdown refreshed!\n${text}`)
+        }, 24 * 60 * 60 * 1000);
+    });
+    updatecount
 }
 
 module.exports = frozen_2_countdown;
