a5db6d6f6db7e10c2ad7f9a65fdba48442c1b2b8
Greep <89matt89.md@gmail.com>
2019-06-15 08:13:36 +0200

    Add status (For donors)

--- bot_nightly.js
+++ bot_nightly.js
@@ -22,28 +22,19 @@ function functiontime() {
     return time
 }
 
+const channel_id = require('./counter/channel_ids.json');
+
 const num_members = require('./counter/member.js');
 const frozen_2_countdown = require('./counter/frozen2.js');
-const raps_birthday_countdown = require('./counter/raps_birthday.js')
-const channel_id = require('./counter/channel_ids.json');
-/*
-Dev-testing category channel ids
-	585767717387370496 - Messages
-	585782174012407848 - Members
-	585834618910015491 - Countdown for frozen II
-    586086472201797681 - Unused
-    586199073292550161 - Raps' Birthday Countdown
-*/
+
 const lant_num_members = () => num_members(client,"570024448371982373", channel_id.members);
 const lant_frozen_II = () => frozen_2_countdown(client, channel_id.frozen2);
-const lant_raps_birth = () => raps_birthday_countdown(client, channel_id.raps_birthday);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!\nOn ${functiondate(0)} at ${functiontime(0)}`);
     client.user.setStatus('dnd');
     lant_num_members();
     lant_frozen_II();
-    lant_raps_birth();
 });
 client.on('guildMemberAdd', member => {
     lant_num_members();
@@ -67,6 +58,9 @@ client.on('message', message => {
     
     const lantern = require('./cmds/lantern.js');
     lantern(message, client, prefix);
+
+    const status = require('./cmds/status.js');
+    status(message, client, prefix);
 
     const screenshot = require('./cmds/screenshots/screenshot.js');
     screenshot(message, client, prefix, functiondate, functiontime,cooldowns);
---
+++ cmds/status.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+
+function status(message, client, prefix) {
+    let donorsonly = new Discord.RichEmbed()
+    donorsonly.setColor("#D30051")
+    .addField("Sorry :shrug:", `This command is reserved for donors only\n\n[You can contribute to the financing of the project by clicking here](https://patreon.com/KingdomCoronaBot)`)
+    .setFooter(`If you have already donated but you don't have the role, type "!bug bot donated but haven't the role"`, `${client.user.avatarURL}`)
+
+    if (message.content.startsWith(prefix + 'setgame')) {
+        if (!message.member.roles.find(r => r.name === "KEY (The Guiding Lanterns)")) return message.channel.send(donorsonly)
+        let args = message.content.split(" ");
+        args.shift();
+        client.user.setActivity(args.join(" "));
+        message.channel.send(`The status of the bot has been set to "Playing **${args.join(" ")}**" !`)
+    }
+    if (message.content.startsWith(prefix + 'setwatch')) {
+        if (!message.member.roles.find(r => r.name === "KEY (The Guiding Lanterns)")) return message.channel.send(donorsonly)
+        let args = message.content.split(" ");
+        args.shift();
+        client.user.setActivity(args.join(" "), { type: 'WATCHING' })
+        message.channel.send(`The status of the bot has been set to "Watching **${args.join(" ")}**" !`)
+    }
+    if (message.content.startsWith(prefix + 'setlisten')) {
+        if (!message.member.roles.find(r => r.name === "KEY (The Guiding Lanterns)")) return message.channel.send(donorsonly)
+        let args = message.content.split(" ");
+        args.shift();
+        client.user.setActivity(args.join(" "), { type: 'LISTENING' })
+        message.channel.send(`The status of the bot has been set to "Listening to **${args.join(" ")}**" !`)
+    }
+}
+
+module.exports = status;
