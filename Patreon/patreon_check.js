c597c3c0fecc1f29f9ee0c00e10a5a8eaa2185c0
Greep <89matt89.md@gmail.com>
2019-06-17 15:15:57 +0200

    Moving files to a dir

---
+++ Patreon/patreon_check.js
@@ -0,0 +1,4 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const db = JSON.parse(fs.readFileSync('./Patreon/patreon_db.json', "utf8"))
+
--- patreon_check.js
+++
@@ -1,4 +0,0 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const db = JSON.parse(fs.readFileSync('./patreon_db.json', "utf8"))
-
