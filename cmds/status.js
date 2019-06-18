07e5bd2057baa299ae3749e4103598a969d50fe0
Greep <89matt89.md@gmail.com>
2019-06-18 07:46:14 +0200

    Add patreon check and database

--- cmds/status.js
+++ cmds/status.js
@@ -1,15 +1,11 @@
 const Discord = require("discord.js");
 
-function status(message, client, prefix) {
-    let donorsonly = new Discord.RichEmbed()
-    donorsonly.setColor("#D30051")
-    .addField("Sorry :shrug:", `This command is reserved for donors only\n\n[You can contribute to the financing of the project by clicking here](https://patreon.com/KingdomCoronaBot)`)
-    .setFooter(`If you have already donated but you don't have the role, type " !bug Donated but haven't the KEY "`, `${client.user.avatarURL}`);
+function status(message, client, prefix, donor, donoronlymsg) {
 
     const nothingsetmsg = `You put nothing? Okay, I set nothing ¯\\_(ツ)_/¯`
 
     if (message.content.startsWith(prefix + 'setgame')) {
-        if (!message.member.roles.find(r => r.name === "KEY (The Guiding Lanterns)")) return message.channel.send(donorsonly)
+        if (!donor) return message.channel.send(donoronlymsg)
         let args = message.content.split(" ");
         args.shift();
         client.user.setActivity(args.join(" "));
@@ -17,7 +13,7 @@ function status(message, client, prefix) {
         else message.channel.send(`The status of the bot has been set to "Playing **${args.join(" ")}**" !`)
     }
     if (message.content.startsWith(prefix + 'setwatch')) {
-        if (!message.member.roles.find(r => r.name === "KEY (The Guiding Lanterns)")) return message.channel.send(donorsonly)
+        if (!donor) return message.channel.send(donoronlymsg)
         let args = message.content.split(" ");
         args.shift();
         client.user.setActivity(args.join(" "), { type: 'WATCHING' })
@@ -25,7 +21,7 @@ function status(message, client, prefix) {
         else message.channel.send(`The status of the bot has been set to "Watching **${args.join(" ")}**" !`)
     }
     if (message.content.startsWith(prefix + 'setlisten')) {
-        if (!message.member.roles.find(r => r.name === "KEY (The Guiding Lanterns)")) return message.channel.send(donorsonly)
+        if (!donor) return message.channel.send(donoronlymsg)
         let args = message.content.split(" ");
         args.shift();
         client.user.setActivity(args.join(" "), { type: 'LISTENING' })
