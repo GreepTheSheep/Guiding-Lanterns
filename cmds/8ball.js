b262d90be40df62033261d3466c8cdf3ee065e45
Greep <89matt89.md@gmail.com>
2019-06-26 17:31:36 +0200

    Mody text instread delete and add a new text, and add random timeout between 0.5 and 10 sec

--- cmds/8ball.js
+++ cmds/8ball.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const answerfile = './cmds/8ball_answers.json'
 
+const wait = require('util').promisify(setTimeout);
+const min=500; 
+const max=10000;
+
 function eight_ball(message, client, prefix){
 
     if(message.content.startsWith(prefix + "tellme")){
@@ -16,11 +20,13 @@ function eight_ball(message, client, prefix){
 
         let reponse = randomItem(reponses);
 
+        var randomwait = Math.random() * (+max - +min) + +min;
+
         if (args.length < 1) {
             message.channel.send('**Ask me a question**');
         } else {
             message.channel.send("Let's see...")
-            .then(msg => msg.delete(3000).then(m=>message.channel.send(`**${reponse}**`))
+            .then(msg => wait(randomwait).then(m=>msg.edit(`**${reponse}**`))
             )
             .catch(e => message.channel.send(':/ Hmm... Looks like there\'s been a error.\nDon\'t worry! The report was sent at the devs!').then(console.log(e)));
         }
