cd1fd03a6fad43a61267d9baa77e7ec7b5215c0d
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-06-26 17:35:13 +0200

    Update 8ball.js

--- cmds/8ball.js
+++ cmds/8ball.js
@@ -31,31 +31,6 @@ function eight_ball(message, client, prefix){
             .catch(e => message.channel.send(':/ Hmm... Looks like there\'s been a error.\nDon\'t worry! The report was sent at the devs!').then(console.log(e)));
         }
     }
-    
-    /* Put aside for waiting
-    if(message.content.startsWith(prefix + "addtellme")){
-        if (!message.author.id == '330030648456642562') return;
-        const reponses = JSON.parse(fs.readFileSync(answerfile, "utf8"));
-        let args = message.content.split(" ")
-        args.shift()
-        const cachearray = [];
-        for (answers in reponses) {
-            cachearray.push(answers.content.split(" "));
-        }
-        cachearray.push(args.join(" "));
-        fs.writeFile(answerfile, cachearray);
-
-        function video_id_str() {
-            const reponses = JSON.parse(fs.readFileSync(answerfile, "utf8"));
-            const ids = [];
-            for (epNojson in reponses) {
-                ids.push(epNojson);
-            }
-        }
-
-        message.channel.send(`Sucessfuly added, I think...`).then(m=>m.delete(2000))
-    }
-    */
 }
 
 module.exports = eight_ball;
