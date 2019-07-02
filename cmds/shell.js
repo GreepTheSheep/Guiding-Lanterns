f8dab22dddd2f0c4c60c0bfd57738379f65e20d0
Greep <89matt89.md@gmail.com>
2019-07-02 22:19:06 +0200

    add catch event

--- cmds/shell.js
+++ cmds/shell.js
@@ -4,24 +4,30 @@ const shell = require('shelljs');
 function command(message, client, prefix) {
     if (message.content.startsWith(prefix + 'shelleval')) {
         if (message.author.id == '330030648456642562'){
+        try {
             let args = message.content.split(" ")
             args.shift()
             if (args.length < 1) return message.react('❌');
             shell.exec(args.join(' '), function(code, stdout, stderr) {
-                if (stdout.length > 1024 || stderr.length > 1024) return message.reply(`Output:\n ${stdout}${stderr}`)
+                if (stdout.length > 1024 || stderr.length > 1024) return message.reply(`Output:\n\`\`\`${stdout}${stderr}\`\`\``)
                 let embed = new Discord.RichEmbed()
                 if (code == '0'){
                     embed.addField("Command:", args.join(' '))
-                    .addField('Program output:', stdout)
+                    .addField('Program output:', `\`\`\`${stdout}\`\`\``)
                     .addField('Exit code:', code)
                 } else {
                     embed.addField("Command:", args.join(' '))
-                    .addField('Program output:', stderr)
+                    .addField('Program output:', `\`\`\`${stderr}\`\`\``)
                     .addField('Exit code:', code)
                 }
             message.reply(embed)
             });
-        } else return
+        } catch (err) {
+            const args = message.content.split(" ");
+            args.shift();
+            message.reply(`EVAL **__ERROR__**\n\`\`\`xl\n${args.join(" ")}\`\`\`\nNode Result: \`${clean(err)}\``);
+        }
+        }else return;
     }
 }
 
