e06375951cf8fc86789eeceb17147c8943be3102
Greep <89matt89.md@gmail.com>
2019-07-02 22:06:21 +0200

    change syntax of ram use

--- cmds/about.js
+++ cmds/about.js
@@ -9,7 +9,7 @@ function about(message, client, prefix) {
         embed.setColor("#9C01C4")
             .addField(client.user.tag, `Version ${package.version}\nLicense: ${package.license}`)
             .addField("Cast:", `- **Greep#3022** : Original idea\n- **Ajam#3536** : Contributor`, true)
-            .addField("Technical information:", `Libary used: [Discord.js](https://discord.js.org)\nDelay between bot and Discord server: ${Math.round(client.ping)} ms\nType of operating System: ${os.type}: ${os.release}\nRAM used: ${Math.round(process.memoryUsage().rss / 1024 / 1000)}/${Math.round(os.totalmem() / 1024 / 1000)} MB (${Math.round(os.freemem() / 1024 / 1000)} MB free)\nBot uptime: ${clientUpTime}`)
+            .addField("Technical information:", `Libary used: [Discord.js](https://discord.js.org)\nDelay between bot and Discord server: ${Math.round(client.ping)} ms\nType of operating System: ${os.type}: ${os.release}\nRAM used: ${Math.round(os.freemem() / 1024 / 1000)}/${Math.round(os.totalmem() / 1024 / 1000)} MB (${client.user.username} uses ${Math.round(process.memoryUsage().rss / 1024 / 1000)} MB)\nBot uptime: ${clientUpTime}`)
             .addField("Useful links:", `[**Kingdom of Corona ☀**](https://discord.gg/BunQeKh)\n[Bot source code on GitHub](https://github.com/GreepTheSheep/Guiding-Lanterns)\n[Developement server](https://discord.gg/5QCQpr9)\n[Support the bot](https://donatebot.io/checkout/570024448371982373?buyer=${message.author.id})`, true)
             .setThumbnail(`${client.user.avatarURL}`)
             .setFooter(`${client.user.username}`, `${client.user.avatarURL}`)
