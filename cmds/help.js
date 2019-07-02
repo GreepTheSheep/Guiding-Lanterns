74f241f3c261df3148a6b518ab5aaf54c7c9c83f
Greep <89matt89.md@gmail.com>
2019-07-02 11:50:02 +0200

    Fix bug

--- cmds/help.js
+++ cmds/help.js
@@ -3,9 +3,8 @@ const Discord = require('discord.js');
 function help(message, client, prefix) {
     if (message.content.startsWith(prefix + 'help') || message.content.startsWith(prefix + 'commands') || message.content.startsWith(`<@!${client.user.id}> help`) || message.content.startsWith(`<@${client.user.id}> help`) || message.content.startsWith(`<@!${client.user.id}> commands`) || message.content.startsWith(`<@${client.user.id}> commands`)) {
         let embed = new Discord.RichEmbed()
-        embed.setTitle('You feel lost? Don\'t worry :wink:')
+        embed.setTitle('You feel lost? Don\'t worry :wink:\n\nList of commands:')
             .setColor("#9C01C4")
-            .addField("List of commands:", ` `)
             .addField("Tangled commands <:heureuse:570820764799074335>", `\`${prefix}quote\` : Send a random quote from Tangled (and the series also)\n\`${prefix}screenshot\` : Take a screenshot from the movie or from the series`, true)
             .addField("Util:", `\`${prefix}wolfram\` : What would you like to know about ?\nSupporter only: \`${prefix}full-wolfram\` : What would you like to know about ? (Get full answer)\nSupporter only: \`${prefix}setgame\` : Sets a "Playing ..." status\nSupporter only: \`${prefix}setwatch\` : same as setgame but is "Watching ..."\nSupporter only: \`${prefix}setlisten\` : Same as setgame but is "Listening to ..."`, true)
             .addField("Fun:", `**NEW** \`${prefix}8ball\` : Ask a question and he will answer.\nSupporter only: \`${prefix}say\` : Makes the bot talking !`, true)
