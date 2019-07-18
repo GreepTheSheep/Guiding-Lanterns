c8d967ce6a7cf905c3e231a30e8e92d7580fdbd9
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-07-18 21:37:24 +0200

    Add link if the user has not voted

--- cmds/voted.js
+++ cmds/voted.js
@@ -6,9 +6,9 @@ function voted(message, client, prefix, dbl) {
         
         dbl.hasVoted(message.author.id).then(voted => {
             if (voted) {
-                message.reply('Yes!')
+                message.reply('Yes, you did!')
             } else {
-                message.reply('No...')
+                message.reply('No, you didn\'t.\nHere\'s the link: https://discordbots.org/bot/569624646475972608/vote')
             }
         });
     }
