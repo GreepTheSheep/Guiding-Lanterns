931d206e30b71b8713f14e769cac6a2450009686
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-08-18 10:54:41 +0200

    Refresh counter every 15 seconds

--- counter/users.js
+++ counter/users.js
@@ -6,12 +6,14 @@ function users_count(client, channel_id) {
         console.log(`Channel: ${channel_id} cannot be found`);
         return;
     }
-
+    setInterval(function(){
     const total = client.users.size;
     const bots = client.users.filter(m => m.bot).size; 
     const members = total - bots
 
     channel.setName(`Users: ${members}`).catch(err => console.log(err));
+    }, 15000)
+    
 }
 
 module.exports = users_count
