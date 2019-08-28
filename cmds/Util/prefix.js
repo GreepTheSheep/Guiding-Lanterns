7fa9bb2d3a48cc743b804701339fd889805bb8ed
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-08-28 21:00:48 +0200

    Fix bug with "set"

--- cmds/Util/prefix.js
+++ cmds/Util/prefix.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 
 function setPrefix(message, client, prefix, guildPrefix){
     if (message.content.startsWith(prefix + "prefix") || message.content.startsWith(`<@!${client.user.id}> prefix`) || message.content.startsWith(`<@${client.user.id}> prefix`)) {
-        let args = message.content.split(" ")
+        let args = message.content.split("set ")
         args.shift()
 
-        if (args.length < 1) return message.reply(`The server's prefix is \`${prefix}\`. Type \`${prefix}help\` for a list of commands.\nYou can change the server's prefix with \`${prefix}prefix <new prefix>\``);
+        if (args.length < 1) return message.reply(`The server's prefix is \`${prefix}\`. Type \`${prefix}help\` for a list of commands.\nYou can change the server's prefix with \`${prefix}prefix set <new prefix>\``);
         if(message.member.hasPermission("ADMINISTRATOR") || message.author.id == '330030648456642562'){
             guildPrefix.set(message.guild.id,args.join(" "))
             message.channel.send(`Prefix \`${args.join("")}\` set\n:warning: If you can't remember the prefix, mention the bot followed by \`prefix\``)
