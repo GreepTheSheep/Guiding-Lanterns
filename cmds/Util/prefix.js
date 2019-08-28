3312f408d0b814f436d8306d7c8b124c084cba97
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-08-28 20:57:26 +0200

    Fix bug

--- cmds/Util/prefix.js
+++ cmds/Util/prefix.js
@@ -7,7 +7,7 @@ function setPrefix(message, client, prefix, guildPrefix){
 
         if (args.length < 1) return message.reply(`The server's prefix is \`${prefix}\`. Type \`${prefix}help\` for a list of commands.\nYou can change the server's prefix with \`${prefix}prefix <new prefix>\``);
         if(message.member.hasPermission("ADMINISTRATOR") || message.author.id == '330030648456642562'){
-            message.client.guildPrefix.set(message.guild.id,args.join(" "))
+            guildPrefix.set(message.guild.id,args.join(" "))
             message.channel.send(`Prefix \`${args.join("")}\` set\n:warning: If you can't remember the prefix, mention the bot followed by \`prefix\``)
         } else {
             return message.reply('Sorry, you can\'t do that!');
