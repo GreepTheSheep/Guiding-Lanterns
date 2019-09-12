ce7abee02089c606f2553f4c5dd849b9af21cf97
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-09-12 18:19:07 +0200

    Add second acc of Greep

--- cmds/Owner/log.js
+++ cmds/Owner/log.js
@@ -4,7 +4,7 @@ const {Attachment} = require('discord.js')
 function claimlog (message, client, prefix) {
     if (message.content.startsWith(prefix + 'log')){
         try {
-            if (message.author.id == '330030648456642562'){
+            if (message.author.id === '330030648456642562'  || message.author.id === "460348027463401472"){
                 if (client.user.id == '577477992608038912') var attachment = new Attachment('./logs/bot_nightly.log')
                 if (client.user.id == '569624646475972608') var attachment = new Attachment('./logs/bot.log')
                 message.channel.send('Log history for ' + client.user.tag, attachment)
