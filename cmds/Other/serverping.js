56791553522302efcd42008bf8ee5f943f4b8e9e
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-10-13 17:34:08 +0200

    Fix bug

--- cmds/Other/serverping.js
+++ cmds/Other/serverping.js
@@ -2,11 +2,10 @@ const Discord = require('discord.js')
 
 async function serverping(message, client, prefix){
     if(message.content.startsWith(prefix + "ping")) {
+        const m = await message.channel.send("Pong?");
         let embed = new Discord.RichEmbed
         embed.setTitle(`Pong!`)
             .setDescription(`❓: ${m.createdTimestamp - message.createdTimestamp}ms\n💓: ${Math.round(client.ping)}ms`)
-
-        const m = await message.channel.send("Pong?");
         m.edit(embed);
       }
 }
