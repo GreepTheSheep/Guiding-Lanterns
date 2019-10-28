b8bd95bdd079cab23c1cd8f30ddf44bd413f9cd4
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-10-28 23:08:01 +0100

    First version

--- events/twitter-starboard.js
+++ events/twitter-starboard.js
@@ -5,18 +5,44 @@ const config = JSON.parse(fs.readFileSync(configfile, "utf8"));
 
 const Twitter = require('twitter')
 const twitter_tokens = {
-    consumer_key:        config.consumer_key,
-    consumer_secret:     config.consumer_secret,
-    access_token:        config.access_token_key,
-    access_token_key:    config.access_token_key,
-    access_token_secret: config.access_token_secret
+    consumer_key:        config.twitter_consumer_key,
+    consumer_secret:     config.twitter_consumer_secret,
+    access_token:        config.twitter_access_token_key,
+    access_token_key:    config.twitter_access_token_key,
+    access_token_secret: config.twitter_access_token_secret
 };
 const twitter = new Twitter(twitter_tokens)
 
-function twitter_starboard (client, reaction, logchannel, date, time){
+async function twitter_starboard (client, reaction, logchannel, date, time){
     try{
         if (reaction.emoji.name === '⭐' && reaction.count === 3) {
-            
+
+            // We use the this.extension function to see if there is anything attached to the message.
+            const image = reaction.message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : ''; 
+  
+            // Load your image
+            var data = fs.readFileSync(image);
+
+            // Make post request on media endpoint. Pass file data as media parameter
+            twitter.post('media/upload', {media: data}, function(error, media, response) {
+                if (!error) {
+                    // Lets tweet it
+                    var status = {
+                        status: reaction.message.content,
+                        media_ids: media.media_id_string // Pass the media id string
+                    }
+
+                    client.post('statuses/update', status, function(error, tweet, response) {
+                        if (!error) {
+                            console.log(tweet);
+                        }
+                    });
+                } else if (error) {
+                    const errmsg = `Twitter Starboard POST error : ${error}`
+                    console.log(`[${date} - ${time}] ${errmsg}`)
+                    logchannel.send(errmsg)
+                }
+            });
         }
     } catch (err) {
         const errmsg = `Twitter Starboard POST error : ${err}`
