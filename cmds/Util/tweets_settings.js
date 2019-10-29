683f9ce1efba509f0d333527ef9f59c6a3da034e
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-10-29 23:24:02 +0100

    !twitter set is set only for admins

--- cmds/Util/tweets_settings.js
+++ cmds/Util/tweets_settings.js
@@ -15,12 +15,14 @@ function twitter_settings(message, client, prefix, lang, logchannel){
             .setColor('#5EA9DD')
             return message.channel.send(embed)
         } else if (args[0] == 'set') {
-            if (tweets_setting.get(message.guild.id) === false){
-                tweets_setting.set(message.guild.id, true)
-                message.channel.send(lang.twitter_send)
-            } else if (tweets_setting.get(message.guild.id) === true){
-                tweets_setting.set(message.guild.id, false)
-                message.channel.send(lang.twitter_no_send)
+            if(message.member.hasPermission("ADMINISTRATOR") || message.author.id == '330030648456642562'){
+                if (tweets_setting.get(message.guild.id) === false){
+                    tweets_setting.set(message.guild.id, true)
+                    message.channel.send(lang.twitter_send)
+                } else if (tweets_setting.get(message.guild.id) === true){
+                    tweets_setting.set(message.guild.id, false)
+                    message.channel.send(lang.twitter_no_send)
+                } else return;
             } else return;
         }
     }
