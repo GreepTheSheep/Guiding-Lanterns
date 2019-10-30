5937c54a4be1bb862ac64224369436205b3d5a1c
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-10-30 22:28:13 +0100

    Add !nobg (dbl vote locked bc of number of requests)

---
+++ cmds/Util/removebg.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js')
+const request = require('request');
+const fs = require('fs');
+const configfile = "./data/config.json";
+const config = JSON.parse(fs.readFileSync(configfile, "utf8"));
+
+function removebgscr(message, client, prefix, logchannel, date, time, lang){
+    const args = message.content.split(/ +/).slice(1);
+    var attachurl
+    if (args.length > 0) attachurl = args[0]
+    if (message.attachments.size > 0) attachurl = message.attachments.array()[0].url
+    if (!attachurl) return message.reply('you have not given a image')
+    let valid = attachurl.endsWith('.png')
+    if (!valid) return message.reply('only .png images are supported. Use https://cloudconvert.com/ if you want to')
+    request.post({
+        url: 'https://api.remove.bg/v1.0/removebg',
+        formData: {
+            image_file: attachurl,
+            size: 'auto',
+        },
+        headers: {
+            'X-Api-Key': config.removebg_key
+        },
+        encoding: null
+    }, function(error, response, body) {
+        if(error) return function(){
+            const errormsg = `Remove.bg Request failed: ${error}`
+            console.error(`[${date()} - ${time()}] ${errormsg}`);
+            logchannel.send(errormsg)
+            message.channel.send(lang.error_reported)
+        }
+        if(response.statusCode != 200) return function(){$
+            const errormsg = `Remove.bg Request failed: Error ${response.statusCode}: ${body.toString('utf8')}`
+            console.error(`[${date()} - ${time()}] ${errormsg}`);
+            logchannel.send(errormsg)
+            message.channel.send(lang.error_reported)
+        }
+         
+        fs.writeFileSync(`./data/${message.guild.id}/no-bg.png`, body);
+        const attachment = new Discord.Attachment(`./data/${message.guild.id}/no-bg.png`)
+        message.channel.send(attachment).then(m=>fs.unlinkSync(`./data/${message.guild.id}/`))
+        });
+}
+
+function removebg(message, client, prefix, logchannel, date, time, lang, dbl){
+    if (message.content.startsWith(prefix + 'removebg') || message.content.startsWith(prefix + 'nobg') || message.content.startsWith(prefix + 'nobackground')){
+        try{
+        if (dbl == undefined) return removebgscr(message, client, prefix, logchannel, date, time, lang)
+        dbl.hasVoted(message.author.id).then(voted => {
+            if (voted) {
+                removebgscr(message, client, prefix, logchannel, date, time, lang)
+            } else {
+                let embed = new Discord.RichEmbed()
+                embed.setTitle('ERROR!')
+                .setColor('#ff0000')
+                .addField("This command is vote locked!", `Have you voted for the bot?\nVoting for the bot keeps the dev. of the bot alive :wink:\n\nhttps://discordbots.org/bot/569624646475972608/vote \n\n(Synchronization with the vote and bot can take about 5 minutes, you can check if you voted with \`${prefix}didivote\`)`)
+                .setFooter(`Type "${prefix}bug <details of your bug>" to send at the devs`, `${message.author.displayAvatarURL}`)
+                message.channel.send(embed)
+            }
+        });
+    } catch (error) {
+        const errormsg = `Remove.bg Request failed: ${error}`
+        console.error(`[${date()} - ${time()}] ${errormsg}`);
+        logchannel.send(errormsg)
+        message.channel.send(lang.error_reported)
+    }
+    }
+}
+
+module.exports = removebg
--- cmds/cmds_index.js
+++ cmds/cmds_index.js
@@ -69,6 +69,9 @@ function cmds_index(message, client, prefix, functiondate, functiontime, cooldow
     const geturlofattachment = require('./Util/geturlofattach.js')
     geturlofattachment(message, client, prefix, lang);
 
+    const removebg = require('./Util/removebg.js')
+    removebg(message, client, prefix, getlogchannel(), functiondate, functiontime, lang, dbl)
+
     const speedtest = require('./Util/speedtest.js')
     speedtest(message, client, prefix, getlogchannel(), dbl)
     
