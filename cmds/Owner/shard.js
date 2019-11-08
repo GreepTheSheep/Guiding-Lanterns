e3bb4ca6e0c35f835893fa0d597612823e433507
Greep <89matt89.md@gmail.com>
2019-11-08 10:53:02 +0100

    shardinfo (first commit)

---
+++ cmds/Owner/shard.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+
+function shardcommands(message, client, prefix){
+    if(message.content.startsWith(prefix + 'shardinfo')){
+        
+    }
+}
+
+module.exports = shardcommands
