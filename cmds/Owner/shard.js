fe8158863753655fa4b53d547eacd067dbeff420
Greep <89matt89.md@gmail.com>
2019-11-08 11:32:48 +0100

    add shardinfo command (wip)

--- cmds/Owner/shard.js
+++ cmds/Owner/shard.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js')
 
 function shardcommands(message, client, prefix){
-    if(message.content.startsWith(prefix + 'shardinfo')){
-        
+    if (message.author.id === '330030648456642562'  || message.author.id === "460348027463401472"){
+        if(message.content.startsWith(prefix + 'shardinfo')){
+            let embed = new Discord.RichEmbed
+            embed.setTitle(`You are on shard ${client.shard.id}/${client.shard.count}`)
+
+        }
     }
 }
 
