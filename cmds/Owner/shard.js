fe4dcded815646e0a2f872b377985d2266a8d0b9
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-11-08 23:52:03 +0100

    Add field with every shard (WIP)

--- cmds/Owner/shard.js
+++ cmds/Owner/shard.js
@@ -1,10 +1,18 @@
 const Discord = require('discord.js')
+const shardManager = new Discord.ShardingManager('../../bot.js')
+const shard = new Discord.Shard(shardManager, client.shard.id)
 
-function shardcommands(message, client, prefix){
+async function shardcommands(message, client, prefix){
     if (message.author.id === '330030648456642562'  || message.author.id === "460348027463401472"){
         if(message.content.startsWith(prefix + 'shardinfo')){
             let embed = new Discord.RichEmbed
             embed.setTitle(`You are on shard ${client.shard.id}/${client.shard.count}`)
+            .setColor('RANDOM')
+            for(var everyShard in shard){
+                const guildsPerShard = await shard.eval(client.guilds.count)
+                embed.addField(`Shard ${everyShard.id}`, `Guilds: ${guildsPerShard}`)
+            }
+
 
             message.channel.send(embed)
         }
