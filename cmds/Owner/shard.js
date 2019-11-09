4a381407bac2578d17dd53b74a8a3198721bd290
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-11-09 12:05:37 +0100

    Update shard.js

--- cmds/Owner/shard.js
+++ cmds/Owner/shard.js
@@ -12,9 +12,9 @@ async function shardcommands(message, client, prefix){
             let embed = new Discord.RichEmbed
             embed.setTitle(`You are on shard ${client.shard.id}/${client.shard.count}`)
             .setColor('RANDOM')
-            for (i = 0; i < client.shard.count; i++){
+            for (i = 0; i === client.shard.count; i++){
                 const guildinshard = await client.shard.fetchClientValues('guilds.size')
-                embed.addField(`Shard #${shard.id}`, `${guildinshard} servers`)
+                embed.addField(`Shard #${shard.id}`, `${guildinshard} servers`, true)
                 var totalguilds = guildinshard.reduce((prev, val) => prev + val, 0)
             }
             embed.addField(`Total servers:`, `${totalguilds} servers`)
