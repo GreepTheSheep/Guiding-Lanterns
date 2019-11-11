c526a55b42bf52dbf8f6803dd249c89f0c2311b1
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-11-11 13:29:22 +0100

    fix bug

--- shard.js
+++ shard.js
@@ -4,7 +4,7 @@ const shard = new ShardingManager('./bot.js',{
   token : config.token
 });
 
-shard.spawn(2).then(console.log(`[SHARD] ${shards.totalShards} total shards started`))
+shard.spawn(2).then(console.log(`[SHARD] ${shard.totalShards} total shards started`))
 
 shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id} started`));
 
