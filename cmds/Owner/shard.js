8bdeb4682b177a1e109ce524197e6196281bce68
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-11-11 13:28:31 +0100

    remove sharding manager for testing

--- cmds/Owner/shard.js
+++ cmds/Owner/shard.js
@@ -4,19 +4,16 @@ const Discord = require('discord.js')
 async function shardcommands(message, client, prefix){
     if (message.author.id === '330030648456642562'  || message.author.id === "460348027463401472"){
         if(message.content.startsWith(prefix + 'shardinfo')){
-            var shardManager 
-            if (client.user.id == '577477992608038912') shardManager = new Discord.ShardingManager('./bot_nigthly.js')
-            if (client.user.id == '569624646475972608') shardManager = new Discord.ShardingManager('./bot.js')
-            if(!shardManager) return message.reply('shardManager is not set').then(m=>m.delete(7000))
-            const shard = new Discord.Shard(shardManager, client.shard.id)
             let embed = new Discord.RichEmbed
             embed.setTitle(`You are on shard ${client.shard.id}/${client.shard.count - 1}`)
             .setColor('RANDOM')
+            
             for (i = 0; i < client.shard.count; i++){
                 const guildinshard = await client.shard.fetchClientValues('guilds.size')
-                embed.addField(`Shard #${shard.id}`, `${guildinshard} servers`, true)
+                //embed.addField(`Shard #${shard.id}`, `${guildinshard} servers`, true)
                 var totalguilds = guildinshard.reduce((prev, val) => prev + val, 0)
-            }
+            } 
+            
             embed.addField(`Total servers:`, `${totalguilds} servers`)
 
 
