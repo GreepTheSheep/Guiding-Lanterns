1e49cf4c5ada0a2c8f36c527bc6271cfe87c4d15
Greep <89matt89.md@gmail.com>
2019-11-23 23:01:48 +0100

    fix lots of bugs, add buy and sell, add items count in inventory

--- cmds/Currency/market.js
+++ cmds/Currency/market.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js')
 const Enmap = require('enmap')
 
-function itemList(cur_json){
+function itemList(message, cur_json){
+    const inv = new Enmap({name:"cur_inventory"})
     const array = [];
     var id = 0
     for (var item of cur_json.item){
-        array.push(`[ ${id} ] ${item.name} - 📥 ${item.cost} ${cur_json.cur.symbol} - 📤 ${item.sell} ${cur_json.cur.symbol}`)
+        if (!inv.has(`${message.author.id}_${id}`)) inv.set(`${message.author.id}_${id}`, 0)
+        array.push(`[ ${id} ] ${item.name} (📦 ${inv.get(`${message.author.id}_${id}`)}) - 📥 ${item.cost} ${cur_json.cur.symbol} - 📤 ${item.sell} ${cur_json.cur.symbol}`)
         id++
     }
     return array.join('\n')
@@ -33,7 +35,7 @@ function market(message, client, prefix, cooldowns, cur_json){
                 totalSeconds %= 3600;
                 let minutes = Math.floor(totalSeconds / 60);
                 let seconds = totalSeconds % 60;
-                return message.reply(`please wait ${hours.toFixed(0)} hour(s), ${minutes.toFixed(0)} minute(s) and ${seconds.toFixed(0)} second(s) before reusing the \`${prefix+'market'}\` command.`)
+                return message.reply(`please wait ${seconds.toFixed(0)} second(s) before reusing the \`${prefix+'market'}\` command.`)
             }
         }
 
@@ -55,36 +57,47 @@ function market(message, client, prefix, cooldowns, cur_json){
             let listembed = new Discord.RichEmbed()
             listembed.setTitle(cur_json.name[2])
                 .setColor("#0567DA")
-                .addField("Avialble items:", itemList(cur_json))
-                .setFooter(`Your balance: ${bal.get(message.author.id)} ${cur_json.cur.symbol}\n📥 is the price of buying, 📤 is the price of selling\nUsage: ${prefix}market <buy|sell> <ID>`)
+                .addField("Avialble items:", itemList(message, cur_json))
+                .setFooter(`Your balance: ${bal.get(message.author.id)} ${cur_json.cur.symbol}\n📦 is the number on your inventory, 📥 is the price of buying, 📤 is the price of selling\nUsage: ${prefix}market <buy|sell> <ID>`)
             return message.channel.send(listembed)
         } else if (args [0] === 'buy' || args[0] === 'sell'){
             if (!args[1]) return message.reply(`Usage: ${prefix}market <buy|sell> <ID> [count]`)
-            if (isNaN(args[1])) return message.reply('You have not given a correct ID, check with \`' + prefix + 'market list\`')
+            if (('' + args[1]).match(/^[\d.]+%$/)) return message.reply('You have not given a correct ID, check with \`' + prefix + 'market list\`')
             const inv = new Enmap({name:"cur_inventory"})
             var count;
+            
             if (!args[2]){
                 count = 1
             } else if (args[2]) {
-                if (isNaN(args[1])) count = 1
-                else if (!isNaN(args[1])) count = args[2]
+                if (isNaN(Number(args[2]))) count = 1
+                else if (!isNaN(Number(args[2]))) {
+                    if (Number(args[2]) === 0) return message.reply('Your [count] argument is invalid, check your arguments.')
+                    count = Number(args[2])
+                }
             }
             
             if (args[0] === 'buy'){
-                if (bal.get(message.author.id) < cur_json.item[args[1]].cost) return message.reply('you don\'t have enough money to buy that.')
+                if (bal.get(message.author.id) < cur_json.item[args[1]].cost * count) return message.reply(`you don\'t have enough money to buy ${count} ${cur_json.item[args[1]].name}.`)
 
                 if (!inv.has(`${message.author.id}_${args[1]}`)) inv.set(`${message.author.id}_${args[1]}`, count)
-                else if (inv.has(`${message.author.id}_${args[1]}`)) inv.set(`${message.author.id}_${args[1]}`, inv.get(message.author.id) + count)
-                
-                bal.set(message.author.id, bal.get(message.author.id) - cur_json.item[args[1]].cost)
 
-                message.reply(`You just bought a *__${cur_json.item[args[1]].name}__** for **${cur_json.item[args[1]].cost} ${cur_json.cur.symbol}**, enjoy!`)
+                inv.set(`${message.author.id}_${args[1]}`, inv.get(`${message.author.id}_${args[1]}`) + count)
+                
+                bal.set(message.author.id, bal.get(message.author.id) - cur_json.item[args[1]].cost * count)
+
+                message.reply(`You just bought __${count} **${cur_json.item[args[1]].name}__** for **${cur_json.item[args[1]].cost * count} ${cur_json.cur.symbol}**, enjoy!`)
 
             } else if (args[0] === 'sell'){
+                if (!inv.has(`${message.author.id}_${args[1]}`)) inv.set(`${message.author.id}_${args[1]}`, 0)
 
+                if (inv.get(`${message.author.id}_${args[1]}`) <= 0) return message.reply(`you can't sell what you don't have, it makes sense to me too.`)
+                
+                if (count > inv.get(`${message.author.id}_${args[1]}`)) count = inv.get(`${message.author.id}_${args[1]}`)
+                
+                inv.set(`${message.author.id}_${args[1]}`, inv.get((`${message.author.id}_${args[1]}`) - count))
+                bal.set(message.author.id, bal.get(message.author.id) + cur_json.item[args[1]].sell * count)
+                message.reply(`You just sold __${count} **${cur_json.item[args[1]].name}__** for **${cur_json.item[args[1]].sell * count} ${cur_json.cur.symbol}**!`)  
             }
-            
-
         } else return message.reply(`Usage: ${prefix}market <buy|sell> <ID> [count]`)
     }
 }
