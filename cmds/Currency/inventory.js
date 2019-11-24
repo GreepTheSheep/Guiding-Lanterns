6e3f907d7ea676f3ace5638f02629196a20ffde7
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-11-24 20:09:42 +0100

    ruser

--- cmds/Currency/inventory.js
+++ cmds/Currency/inventory.js
@@ -66,7 +66,8 @@ function inventory(message, client, prefix, cooldowns, cur_json){
                 .setDescription(itemList(message, cur_json))
                 .setFooter(`Your balance: ${bal.get(message.author.id)} ${cur_json.cur.symbol}`)
             return message.channel.send(listembed)
-        } else if (args[0].startsWith(`<@`).endsWith('>')){
+        } else if (args[0].startsWith(`<`).endsWith('>')){
+            const user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
             
         }
         } else {
