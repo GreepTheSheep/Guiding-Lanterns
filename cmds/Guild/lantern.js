1ca79dda427d33847263922c0c3ca6696f1decc0
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-12-08 12:00:56 +0100

    Moving database to enmap

--- cmds/Guild/lantern.js
+++ cmds/Guild/lantern.js
@@ -1,17 +1,11 @@
 const Discord = require('discord.js');
-const fs = require('fs');
-const countfile = "./data/lanterns.json";
-const lancount = JSON.parse(fs.readFileSync(countfile, "utf8"));
-
+const Enmap = require('enmap')
+const counter = new Enmap({name: 'r-tangled_lantern'})
 
 function lantern(message, client, prefix, getlogchannel, cooldowns) {
 
     if (message.content.includes('<:Lantern:570822664789426186>')) {
-        lancount.count++
-
-        let countwrite = { "count": `${lancount.count}` };
-        let data = JSON.stringify(countwrite);
-        fs.writeFileSync(countfile, data);
+        counter.set('count', counter.get('count') + 1 )
     };
 
     if (message.content.startsWith(prefix + 'lanterns')) {
@@ -49,18 +43,15 @@ function lantern(message, client, prefix, getlogchannel, cooldowns) {
 
         let embed = new Discord.RichEmbed()
         embed.setColor("#E35D05")
-            .addField("Lantern counter", `**__${lancount.count}__ lanterns thrown**`)
+            .addField("Lantern counter", `**__${counter.get('count')}__ lanterns thrown**`)
             .setThumbnail(`https://i.pinimg.com/originals/ed/d8/1f/edd81f0f8cd57690777f917a514ae840.gif`)
             .setFooter(`Launch a lantern with :Lantern: emoji`, `${client.user.avatarURL}`)
-        message.channel.send({ embed: embed })
+        message.channel.send(embed)
     }
 
     if (message.content.startsWith(prefix + 'resetlanterncount')) {
         if (message.author.id == "330030648456642562") {
-            let countwrite = { "count": `0` };
-            let data = JSON.stringify(countwrite);
-            fs.writeFileSync(countfile, data);
-            lancount.count = 0;
+            counter.set('count', 0 )
             console.log('Lantern counter reset !')
             getlogchannel.send('Lantern counter reset !')
 
