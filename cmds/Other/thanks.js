aa142dc70cc75ce79bcc38cf81d8abf464ddde7a
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-01-08 22:19:22 +0100

    Remove "thank eugene" because of a big drama happened to Greep

--- cmds/Other/thanks.js
+++ cmds/Other/thanks.js
@@ -10,11 +10,14 @@ const responses = [
     'You\'re welcome!',
     'https://tenor.com/view/youre-welcome-gif-15359846',
     'https://tenor.com/view/maui-moana-youre-welcome-gif-9587693',
-    'https://tenor.com/view/stevecarrell-gif-7720100'
+    'https://tenor.com/view/stevecarrell-gif-7720100',
+    'Thanks yourself!',
+    'for what?',
+    'Awww'
 ]
 
 function replyToThanks(message, client, prefix){
-    if(message.content.toLowerCase().includes(`thanks <@${client.user.id}>`) || message.content.toLowerCase().includes(`thanks <@!${client.user.id}>`) || message.content.toLowerCase().includes(`thank lantern`) || message.content.toLowerCase().includes(`thank eugene`)){
+    if(message.content.toLowerCase().includes(`thanks <@${client.user.id}>`) || message.content.toLowerCase().includes(`thanks <@!${client.user.id}>`) || message.content.toLowerCase().includes(`thank lantern`)){
         
         function randomItem(array) {
             return array[Math.floor(Math.random() * array.length)];
