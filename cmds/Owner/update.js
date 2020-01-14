5eac4dae8eb0b0eb1b2bc406fea4c505c551d382
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-01-14 19:54:28 +0100

    fix bug

--- cmds/Owner/update.js
+++ cmds/Owner/update.js
@@ -7,13 +7,13 @@ function update(message, client, prefix) {
         if (message.author.id === '330030648456642562'  || message.author.id === "460348027463401472"){
         try {
             message.channel.startTyping()
-            if (client.user.id == '569624646475972608') shell.exec('pm2 stop bot.ecosystem.config.js && git pull && npm install && pm2 start bot.ecosystem.config.js', {silent:true}, function(code, stdout, stderr) {
+            if (client.user.id == '569624646475972608') shell.exec('git pull && npm install && pm2 reload bot.ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
                 message.reply(`Output:\n\`\`\`${stdout}${stderr}\`\`\``).then(m=>message.channel.stopTyping(true));
             });
-            else if (client.user.id == '569624646475972608') shell.exec('pm2 stop bot_nightly.ecosystem.config.js && git pull && npm install && pm2 start bot_nightly.ecosystem.config.js', {silent:true}, function(code, stdout, stderr) {
+            else if (client.user.id == '569624646475972608') shell.exec('git pull && npm install && pm2 reload bot_nightly.ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
                 message.reply(`Output:\n\`\`\`${stdout}${stderr}\`\`\``).then(m=>message.channel.stopTyping(true));
             });
-            else return message.reply('Not supported yet, please execute commands `pm2 stop bot.ecosystem.config.js && git pull && npm install && pm2 start bot.ecosystem.config.js` with !ssh command')
+            else return message.reply('Not supported yet, please execute commands `git pull && npm install && pm2 reload bot.ecosystem.config.js` with !ssh command')
         } catch (err) {
             message.reply(`EVAL **__ERROR__**\n\`\`\`xl\n'pm2 stop GL && git pull && npm install && pm2 start GL'\`\`\``);
             message.channel.stopTyping(true)
