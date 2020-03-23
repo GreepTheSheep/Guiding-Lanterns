0fcf7799837b64f67bd9c28e2ea05bf626d474b9
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-03-23 22:00:21 +0100

    Update themeparks.js

--- cmds/Util/themeparks.js
+++ cmds/Util/themeparks.js
@@ -15,7 +15,7 @@ function parkslist(){
 
 async function parktimes(message, client, prefix, cooldowns){
     if (message.content.startsWith(prefix + 'ridetime')){
-        Themeparks.Settings.Cache = __dirname + "/data/themeparks.sqlite";
+        Themeparks.Settings.Cache = "./data/themeparks.sqlite";
 
          //Implement cooldown
     if (!cooldowns.has(prefix + 'ridetime')) {
