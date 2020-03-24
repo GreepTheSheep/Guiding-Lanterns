4f746150c670cf4bcc1b5a2c0eeab5c97485aea0
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-03-24 17:51:35 +0100

    Update themeparks.js

--- cmds/Util/themeparks.js
+++ cmds/Util/themeparks.js
@@ -44,13 +44,29 @@ async function parktimes(message, client, prefix, cooldowns){
 
         message.channel.send('NOTE: This is work in progress...')
         
-        const awaitmsg = await message.channel.send('Please enter your themepark name')
+        const awaitmsg = await message.channel.send('Please enter your themepark name. Or enter \`list\`')
         const filter = m => message.author == m.author;
         const collectorguild = message.channel.createMessageCollector(filter, {time: 60000, max: 1});
         collectorguild.on('collect', async m => {
             const pleasewait = await message.channel.send('Please wait...')
-            await wait(5000)
-            pleasewait.edit('Working in progress, need some waiting ¯\\_(ツ)_/¯')
+            
+            if(m.content.toLowerCase() == 'list'){
+                const Parks = {};
+                for (const park in Themeparks.Parks) {
+                    Parks[park] = new Themeparks.Parks[park]();
+                }
+                const parkslist = []
+                for (const park in Parks) {
+                    parkslist.push(Parks[park].Name);
+                }
+                embed.setTitle('List of parks:')
+                .setDescription(parkslist.join('\n'))
+                .setFooter('Please type correctely the name of your park!')
+                message.channel.send(embed)
+            } else {
+                message.channel.send('Work in progress. Sorry!')
+            }
+            pleasewait.delete()
         });
         collector4.on('end', (collected, reason) => {
             if (reason == 'time'){
