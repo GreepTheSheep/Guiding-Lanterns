555494d1d88298064b3d2b4a54ad7e86d2a57e54
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-06-15 09:17:14 +0200

    Add reset log by command

--- cmds/Owner/log.js
+++ cmds/Owner/log.js
@@ -1,12 +1,26 @@
 const Discord = require('discord.js')
 const {Attachment} = require('discord.js')
+const fs = require('fs')
 
 function claimlog (message, client, prefix, config) {
     if (message.content.startsWith(prefix + 'log')){
         try {
-            if (client.user.id == '577477992608038912') var attachment = new Attachment('./logs/bot_nightly.log')
-            else var attachment = new Attachment('./logs/bot.log')
-            message.channel.send('Log history for ' + client.user.tag, attachment)
+            let args = message.content.split(" ")
+            args.shift()
+            if (args.length < 1){
+                if (client.user.id == '577477992608038912') var attachment = new Attachment('./logs/bot_nightly.log')
+                else var attachment = new Attachment('./logs/bot.log')
+                return message.channel.send('Log history for ' + client.user.tag, attachment)
+            }
+            if (args[0].toLowerCase() == 'reset'){
+                if (client.user.id == '577477992608038912') var attachment = new Attachment('./logs/bot_nightly.log')
+                else var attachment = new Attachment('./logs/bot.log')
+                message.channel.send('Log history for ' + client.user.tag + '\nThe file will be erased', attachment)
+                if (client.user.id == '577477992608038912') fs.writeFileSync('./logs/bot_nightly.log', '')
+                else fs.writeFileSync('./logs/bot.log', '')
+                console.log('Log file erased by command')
+            }
+                
         } catch (err) {
             console.log(err)
         }
