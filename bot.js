51a5a3e856d7a74a8d5bf3ab33dc1bfbe00603db
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-08-08 12:49:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -263,6 +263,7 @@ client.on('guildDelete', async guild => { // If the bot leave a server
 })
 
 client.on('messageReactionAdd', reaction, user => {
+    console.log(`${user.tag} reacted to a message with the emoji ${reaction.emoji.name}`)
     if (client.user.id == config.public && reaction.message.guild.id == '562602234265731080') { // r/Tangled
         const role_react_accept_rules = require('./events/react-accept-rules-welcome.js')
         role_react_accept_rules(client, reaction, user, getlogchannel(), functiondate(), functiontime())
