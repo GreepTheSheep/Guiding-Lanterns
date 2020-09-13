60a98979db61f68c059dfc5eea2e6a0a879be8e1
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-09-13 10:50:56 +0200

    Update update.js

--- cmds/Owner/update.js
+++ cmds/Owner/update.js
@@ -6,15 +6,15 @@ function update(message, client, prefix) {
     if (message.content.startsWith(prefix + 'update')) {
         try {
             message.channel.startTyping()
-            if (client.user.id == '569624646475972608') shell.exec('git pull && npm update && pm2 reload bot.ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
+            if (client.user.id == '569624646475972608') shell.exec('git pull && sudo npm update && pm2 reload bot.ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
                 message.reply(`Output:\n\`\`\`${stdout}${stderr}\`\`\``).then(m=>message.channel.stopTyping(true));
             });
-            else if (client.user.id == '569624646475972608') shell.exec('git pull && npm update && pm2 reload bot_nightly.ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
+            else if (client.user.id == '569624646475972608') shell.exec('git pull && sudo npm update && pm2 reload bot_nightly.ecosystem.config.js', {silent:false}, function(code, stdout, stderr) {
                 message.reply(`Output:\n\`\`\`${stdout}${stderr}\`\`\``).then(m=>message.channel.stopTyping(true));
             });
-            else return message.reply('Not supported yet, please execute commands `git pull && npm update && pm2 reload bot.ecosystem.config.js` with !ssh command')
+            else return message.reply('Not supported yet, please execute commands `git pull && sudo npm update && pm2 reload bot.ecosystem.config.js` with !ssh command')
         } catch (err) {
-            message.reply(`EVAL **__ERROR__**\n\`\`\`xl\n'pm2 stop GL && git pull && npm update && pm2 start GL'\`\`\``);
+            message.reply(`EVAL **__ERROR__**\n\`\`\`xl\n'git pull && sudo npm update && pm2 start GL'\`\`\``);
             message.channel.stopTyping(true)
         }
     }
