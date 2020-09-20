f7d073a2347ce0f21e1f3d111723b3af0bdb49fe
Matthieu <89matt89.md@gmail.com>
2020-09-20 20:54:56 +0200

    Update tangled-raps-book.js

--- cmds/Fun/images/text/tangled/tangled-raps-book.js
+++ cmds/Fun/images/text/tangled/tangled-raps-book.js
@@ -50,10 +50,17 @@ function tangled_raps_book(message, client, prefix, functiondate, functiontime, 
         } else {
             var lines = []
             text = args.join(' ')
-            
+            if (text.length < 15) lines.push(text)
+            else if (text.length >= 15){
+                lines.push(text.slice(0, 15))
+                for (var i = 0; i = 5; i++) {
+                    if (lines[i].length >= 15) lines.push(text.slice(15, 15))
+                }
+            } 
+            text = lines.join('-\n')
             gm(request(imgurl))
             .font(__dirname + "/Letters_for_Learners.ttf", 35)
-            .drawText(200, 230, args.join(' '))
+            .drawText(200, 230, text)
             .write(process.cwd() + "/data/images/rapsbook.png", function (err) {
                 if (err){
                     message.reply(lang.error_reported)
