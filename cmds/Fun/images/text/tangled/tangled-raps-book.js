36ce8d0ae9ac077e51cb985f14461fccb7517eda
Matthieu <89matt89.md@gmail.com>
2020-09-30 16:12:44 +0200

    Update tangled-raps-book.js

--- cmds/Fun/images/text/tangled/tangled-raps-book.js
+++ cmds/Fun/images/text/tangled/tangled-raps-book.js
@@ -59,22 +59,23 @@ function tangled_raps_book(message, client, prefix, functiondate, functiontime, 
                 var i = 0
                 while (text.length >= lengthLine * i+1) {
                     if (lines[i].charAt(lengthLine-1) == ' '){
-                        console.log('Crée la ligne 1')
+                        console.log('1 Crée la ligne' + i)
                         // Crée une ligne
                         lines.push(lines[i].slice(0, lengthLine-1))
                     } else {
                         if (lines[i].charAt(lengthLine) == ' '){
-                            console.log('Crée la ligne 2')
+                            console.log('2 Crée la ligne' + i)
                             // Crée une ligne
                             lines.push(lines[i].slice(0, lengthLine))
                         } else {
-                            console.log('Crée un tiret puis la ligne 3')
+                            console.log('3 Crée un tiret puis la ligne' + i)
                             // Crée un tiret puis une ligne
                             lines.push(lines[i].concat('-').slice(0, lengthLine))
                         }
                     }
                     i++
                 }
+                console.log(lines)
                 lines.shift()
                 text = lines.join('\n')
             }
