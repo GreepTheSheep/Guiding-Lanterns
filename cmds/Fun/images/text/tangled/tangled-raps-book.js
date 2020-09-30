37502adabc12802fc7890975cf29df729a38bff7
Matthieu <89matt89.md@gmail.com>
2020-09-30 15:35:15 +0200

    Update tangled-raps-book.js

--- cmds/Fun/images/text/tangled/tangled-raps-book.js
+++ cmds/Fun/images/text/tangled/tangled-raps-book.js
@@ -76,7 +76,7 @@ function tangled_raps_book(message, client, prefix, functiondate, functiontime, 
                         }
                     }
                     i++
-                } while (lengthLine * i <= text.length)
+                } while (lengthLine * (i-1) <= text.length)
                 lines.shift()
                 text = lines.join('\n')
             }
