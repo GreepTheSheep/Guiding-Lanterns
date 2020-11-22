1a2013ba5a4f1bbee20d006cc80f56802ee95aaf
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-11-22 12:43:26 +0100

    Update play.js

--- cmds/Music/play.js
+++ cmds/Music/play.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js')
+const DiscordPlayer = require('discord-player')
 
 module.exports = function(message, client, prefix, functiondate, functiontime, cooldowns, getlogchannel, guildPrefix, userLang, lang, langtext, config){
     if (message.content.toLowerCase().startsWith(prefix+'play')){
         let args = message.content.split(" ");
         args.shift();
+        console.log('[Music] command play called. Args: ' + args.join(' '))
         client.player.play(message, args.join(' '), message.member.user);
     }
 }
