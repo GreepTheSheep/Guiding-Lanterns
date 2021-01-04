3e222262bd0b77ada47ce55d576f07d8d99c0d0f
Matthieu <89matt89.md@gmail.com>
2021-01-04 14:56:50 +0100

    Update reddit

--- cmds/Fun/reddit.js
+++ cmds/Fun/reddit.js
@@ -3,32 +3,43 @@ const request = require('request')
 const moment = require('moment')
 const tz = require('moment-timezone')
 
-module.exports = function(message, client, prefix, config){
+var tries = 0
+function checkImage(message, args, prefix){
+    if (tries >= 20) return message.reply('No post with an image was found in this subreddit')
+    request(`https://www.reddit.com${args.length < 1 ? '' : `/r/${args[0]}`}/random.json`, function (error, response, body) {
+        if (error){
+            return message.channel.send('error:'+ error);
+        } else {
+            body = JSON.parse(body)
+            if (Object.prototype.toString.call(body) === '[object Array]'){
+                if (body[0] == undefined) return message.reply('Subreddit not found, check the name')
+                resData = body[0].data.children[0].data
+                if (resData.post_hint !== 'image'){
+                    tries++
+                    checkImage(message, args, prefix)
+                } else postEmbed(message, args, prefix, resData)
+            } else if (Object.prototype.toString.call(body) === '[object Object]'){
+                if (json.error) return message.reply('Error ' + json.error + ': ' + json.message)
+                else return message.reply('Error :(')
+            }
+        }
+    });
+}
+function postEmbed(message, args, prefix, resData){
+    let embed = new Discord.MessageEmbed
+    embed.setAuthor(`Random post of r/${resData.subreddit}`)
+    .setTitle(resData.title.substring(0, 250))
+    .setDescription(`[Open link](https://reddit.com${resData.permalink})`)
+    .setFooter(`By u/${resData.author}${resData.author_flair_text !== null ? ` - ${resData.author_flair_text}` : ''}`)
+    .setImage(resData.url)
+    if (args.length < 1) return message.channel.send(`Better usage: \`${prefix}reddit [subreddit name]\``, embed)
+    else return message.channel.send(embed)
+}
+
+module.exports = function(message, prefix){
     if (message.content.toLowerCase().startsWith(prefix + 'reddit')){
         let args = message.content.split(" ");
         args.shift();
-        request(`https://www.reddit.com${args.length < 1 ? '' : `/r/${args[0]}`}/random.json`, function (error, response, body) {
-            if (error){
-                console.error('error:', error);
-            } else {
-                console.log('statusCode:', response && response.statusCode);
-                console.log('body:', body);
-                if (Object.prototype.toString.call(body) === '[object Array]'){
-                    if (body[0] == undefined) return message.reply('Subreddit not found, check the name')
-                    var resData = body[0].data.children[0].data
-                }else if (Object.prototype.toString.call(body) === '[object Object]'){
-                    if (json.error) return message.reply('Error ' + json.error + ': ' + json.message)
-                    else return message.reply('Error :(')
-                }
-                if (!resData) return message.reply('No post with an image was found in this subreddit')
-                let embed = new Discord.MessageEmbed
-                embed.setAuthor(`Random post of r/${resData.subreddit}`)
-                .setTitle(resData.title)
-                .setFooter(`By u/${resData.author}${resData.author_flair_text !== null ? ` - ${resData.author_flair_text}` : ''}`)
-                .setImage(resData.url)
-                if (args.length < 1) message.channel.send(`Better usage: \`${prefix}reddit [subreddit name]\``, embed)
-                else message.channel.send(embed)
-            }
-        });
+        checkImage(message, args, prefix)
     }
 }
--- cmds/cmds_index.js
+++ cmds/cmds_index.js
@@ -47,7 +47,7 @@ module.exports = function(message, client, prefix, config, functiondate, functio
 
     require('./Fun/ping.js')(message, client, prefix, config);
 
-    require('./Fun/reddit.js')(message, client, prefix, config);
+    require('./Fun/reddit.js')(message, prefix);
 
     require('./Fun/sus.js')(message, client, prefix);
 
